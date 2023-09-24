5048142aeceefdb39119203a81ee016f0a8806fa
Adrienne Kecskes <akecs619@yahoo.com>
2023-09-24 02:39:28 -0500

    add project cards

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,8 +1,8 @@
 function Projects () {
     return (
       <div className="container mx-auto">
-        <div className="grid grid-cols-2 grid-rows-3 gap-4">
-          <div className="card w-96 bg-base-100 shadow-xl">
+        <div className="grid grid-cols-2 grid-rows-3 gap-5">
+          <div className="card w-96 h-50 bg-base-100">
             <div className="card-body">
               <h2 className="card-title">Travelogue Project</h2>
               <p>Interactive Full-Stack Application</p>
@@ -33,21 +33,120 @@ function Projects () {
                   alt="Sequelize"
                 />
               </figure>
-              <div className="card-actions justify-end">
+              <div className="card-actions p-4 justify-end">
                 <button className="btn btn-primary">Link</button>
                 <button className="btn btn-primary">Repo</button>
               </div>
             </div>
           </div>
-          <div className="card w-96 bg-base-100 shadow-xl">
+          <div className="card w-96 h-50 bg-base-100">
             <figure>
-              <img src="/src/assets/images/Travelogue.png" alt="Shoes" />
+              <img
+                className="object-cover"
+                src="/src/assets/images/Travelogue.png"
+                alt="travelogue"
+              />
             </figure>
           </div>
-          <div>Project 2 Image</div>
-          <div>Project 2 Text</div>
-          <div>Project 3 Text</div>
-          <div>Project 3 Image</div>
+
+          <div className="card w-96 h-50 bg-base-100">
+            <figure>
+              <img
+                className="object-cover h-50"
+                src="/src/assets/images/WOD.png"
+                alt="travelogue"
+              />
+            </figure>
+          </div>
+
+          <div className="card w-96 h-50 bg-base-100">
+            <div className="card-body">
+              <h2 className="card-title">Wheels of Date Night Project</h2>
+              <p>Node.js Application</p>
+              <figure>
+                <img
+                  className="h-10 p-1"
+                  src="/src/assets/images/JSLogo.png"
+                  alt="JS"
+                />
+                <img
+                  className="h-10 p-1"
+                  src="/src/assets/images/htmlLogo.jpeg"
+                  alt="handlebars"
+                />
+                <img
+                  className="h-10 p-1"
+                  src="/src/assets/images/cssLogo.jpeg"
+                  alt="nodejs"
+                />
+                <img
+                  className="h-10 p-1"
+                  src="/src/assets/images/materializeLogo.jpeg"
+                  alt="mysql"
+                />
+                <img
+                  className="h-10 p-1"
+                  src="/src/assets/images/spoonLogo.png"
+                  alt="Sequelize"
+                />
+                <img
+                  className="h-10 p-1"
+                  src="/src/assets/images/movieLogo.jpeg"
+                  alt="Sequelize"
+                />
+              </figure>
+              <div className="card-actions p-4 justify-end">
+                <button className="btn btn-primary">Link</button>
+                <button className="btn btn-primary">Repo</button>
+              </div>
+            </div>
+          </div>
+          <div className="card w-96 h-50 bg-base-100">
+            <figure>
+              <img
+                className="h-50 m-8 object-cover"
+                src="/src/assets/images/techblog.png"
+                alt="travelogue"
+              />
+            </figure>
+          </div>
+          <div className="card w-96 h-50 bg-base-100">
+            <div className="card-body">
+              <h2 className="card-title">Travelogue Project</h2>
+              <p>Interactive Full-Stack Application</p>
+              <figure>
+                <img
+                  className="h-10 p-1"
+                  src="/src/assets/images/JSLogo.png"
+                  alt="JS"
+                />
+                <img
+                  className="h-10 p-1"
+                  src="/src/assets/images/handlebarsLogo.png"
+                  alt="handlebars"
+                />
+                <img
+                  className="h-10 p-1"
+                  src="/src/assets/images/nodejsLogo.png"
+                  alt="nodejs"
+                />
+                <img
+                  className="h-10 p-1"
+                  src="/src/assets/images/mysqlLogo.jpg"
+                  alt="mysql"
+                />
+                <img
+                  className="h-10 p-1"
+                  src="/src/assets/images/sequelizeLogo.png"
+                  alt="Sequelize"
+                />
+              </figure>
+              <div className="card-actions p-4 justify-end">
+                <button className="btn btn-primary">Link</button>
+                <button className="btn btn-primary">Repo</button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     );
