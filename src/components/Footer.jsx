0df27399f71ec92c81aeb9ae6d8b2a3b33ba5409
Adrienne Kecskes <akecs619@yahoo.com>
2023-09-29 17:18:05 -0500

    footer links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,45 +1,43 @@
-function Footer () {
+import { footerItems } from "../lib/content/footerItems";
+function Footer() {
   return (
     <>
       <footer className="footer mt-16 items-center p-4 bg-neutral text-neutral-content">
         <aside className="items-center grid-flow-col">
-         <p className="text-2xl font-bold pl-4">Contact Me</p>
+          <p className="text-2xl font-bold pl-4">Contact Me</p>
         </aside>
         <nav className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
           <div className="flex items-center h-10">
-            <div>
+            <a href="tel://5126533573">
               <img
                 className="footer-icon"
                 src="src/assets/images/phoneIcon.png"
                 alt="phone"
               />
-            </div>
-            <div>
+            </a>
+            <a href="mailto://akecs619@yahoo.com">
               <img
                 className="footer-icon"
                 src="/src/assets/images/contact/emailicon.png"
                 alt="email"
               />
-            </div>
-            <div>
+            </a>
+            <a href="https://www.linkedin.com/in/akecs/">
               <img
                 className="footer-icon"
                 src="/src/assets/images/contact/LinkedinLogo.png"
                 alt="LinkedIn"
               />
-            </div>
+            </a>
 
-            <div  >
-              <img
-                className="footer-icon"
-                src="/githubLogo.png"
-                alt="github"
-              />
-            </div>
+            <a href="https://github.com/akecs512">
+              <img className="footer-icon" src="/githubLogo.png" alt="github" />
+            </a>
           </div>
         </nav>
       </footer>
     </>
-  );}
+  );
+}
 
 export default Footer;
---
+++ src/lib/content/footerItems.js
@@ -0,0 +1,1 @@
+export const footerItems = ["foo", "bar", "baz"];
