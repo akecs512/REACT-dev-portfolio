a817a08a1aac8a89dcdb171f1389d38d5772848b
Adrienne Kecskes <akecs619@yahoo.com>
2023-09-29 20:33:08 -0500

    resume page

--- src/components/SkillsCard.jsx
+++ src/components/SkillsCard.jsx
@@ -1,23 +1,20 @@
-function SkillsCard ({items}) {
-    const {icons} = items;
+/* eslint-disable react/prop-types */
+function SkillsCard({ items }) {
+  const { icons } = items;
 
-    return (
-      <>
-        <div className="flex">
-          {icons.map((icon, index) => (
-            <img
-              className="h-20 p-2"
-              src={icon.path}
-              alt={icon.alt}
-              key={index}
-            />
-          ))}
-        </div>
-      </>
-    );
-
-
-
-
+  return (
+    <>
+      <div className="flex">
+        {icons.map((icon, index) => (
+          <img
+            className="h-20 p-2"
+            src={icon.path}
+            alt={icon.alt}
+            key={index}
+          />
+        ))}
+      </div>
+    </>
+  );
 }
 export default SkillsCard;
--- src/pages/ResumePage.jsx
+++ src/pages/ResumePage.jsx
@@ -1,7 +1,37 @@
+import { pdfjs, Document, Page } from "react-pdf";
+
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  "pdfjs-dist/build/pdf.worker.min.js",
+  import.meta.url
+).toString();
+
 export const ResumePage = () => {
   return (
     <>
       <h1>Resume</h1>
+      <div className="container">
+        <div className="flex flex-col items-start">
+          <button className="btn btn-accent my-4 ">
+            <a href="./public/Adrienne Kecskes Resume 2023.pdf">
+              Download My Resume
+            </a>
+          </button>
+          <Document file="./public/Adrienne Kecskes Resume 2023.pdf">
+            <div className="flex flex-row">
+              <Page
+                renderAnnotationLayer={false}
+                renderTextLayer={false}
+                pageNumber={1}
+              />
+              <Page
+                renderAnnotationLayer={false}
+                renderTextLayer={false}
+                pageNumber={2}
+              />
+            </div>
+          </Document>
+        </div>
+      </div>
     </>
   );
 };
