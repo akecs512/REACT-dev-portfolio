473bd94239e814b7485b9423251f1cf48331464f
Adrienne Kecskes <akecs619@yahoo.com>
2023-09-29 19:59:15 -0500

    style contact form

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -22,33 +22,37 @@ export default function ContactForm() {
       e.target.reset();
   };
   return (
-    <div className="block max-w-sm rounded-lg bg-white p-6 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
-      <form onSubmit={handleSubmit}>
+    <>
+    <h1>Contact Me</h1>
+    <div className="container mx-auto mt-8">
+      <form className="flex flex-col p-10 gap-2 w-full bg-secondary" onSubmit={handleSubmit}>
         <input
           type="text"
           name="name"
           placeholder="Name"
-          className="input input-bordered w-full max-w-xs"
+          className="input input-bordered w-full max-w-sm self-center"
         />
         <input
           type="text"
           name="email"
           placeholder="Email"
-          className="input input-bordered w-full max-w-xs"
+          className="input input-bordered w-full max-w-sm"
         />
         <span>{emailError}</span>
         <textarea
-          className="textarea textarea-bordered" 
+          className="input w-full h-32 textarea-bordered max-w-sm"
           placeholder="Message"
           name="message"
         ></textarea>
-        <div>
-          <span>{messageError}</span>
+        <span>{messageError}</span>
+
+        <div className="self-center">
           <button type="submit" className="btn btn-primary">
             Submit
           </button>
         </div>
       </form>
-    </div>
+      </div>
+      </>
   );
 }
