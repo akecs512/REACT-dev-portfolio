b7bd0c5908fd500059b65a88a79bed966fd88fa8
Adrienne Kecskes <akecs619@yahoo.com>
2023-10-04 10:22:16 -0500

    project card category

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -14,49 +14,39 @@ function Projects() {
       <h1 className="mb-4" id="projects">
         Projects
       </h1>
-  
+<h3 className="text-4xl pt-8 text-accent">----Collaborative Projects----</h3>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-8">
+        
+        <ProjectCard
+          title="Travelogue Application"
+          subtitle="Interactive Full-Stack Application"
+          items={travelogueItems}
+        />
+        <ProjectCard
+          title="Wheels of Date Night Application"
+          subtitle="Node.js Application"
+          items={wodItems}
+        />
+        <ProjectCard title="Coming Soon!" subtitle="" items={projectItems} />
+      </div>
+      <h3 className="text-4xl pt-8 text-accent">----Personal Projects----</h3>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-8">
+        <ProjectCard
+          title="Tech-Blog Application"
+          subtitle="A CMS-style blog site"
+          items={techblogItems}
+        />
 
-        {/* <div className="project-card"> */}
-          <ProjectCard
-            title="Travelogue Application"
-            subtitle="Interactive Full-Stack Application"
-            items={travelogueItems}
-          />
-        {/* </div> */}
-        {/* <div className=""> */}
-          <ProjectCard
-            title="Wheels of Date Night Application"
-            subtitle="Node.js Application"
-            items={wodItems}
-          />
-        {/* </div>  */}
-        {/* <div className=""> */}
-          <ProjectCard title="Coming Soon!" subtitle="" items={projectItems} />
-        {/* </div> */}
-
-        {/* <div className=""> */}
-          <ProjectCard
-            title="Tech-Blog Application"
-            subtitle="A CMS-style blog site"
-            items={techblogItems}
-          />
-        {/* </div> */}
-
-        {/* <div className=""> */}
-          <ProjectCard
-            title="Weather Dashboard Application"
-            subtitle="Server-Side API Application"
-            items={weatherItems}
-          />
-        {/* </div> */}
-        {/* <div className=""> */}
-          <ProjectCard
-            title="Note Taker"
-            subtitle="Express.js Application"
-            items={noteItems}
-          />
-        {/* </div> */}
+        <ProjectCard
+          title="Weather Dashboard Application"
+          subtitle="Server-Side API Application"
+          items={weatherItems}
+        />
+        <ProjectCard
+          title="Note Taker"
+          subtitle="Express.js Application"
+          items={noteItems}
+        />
       </div>
     </>
   );
