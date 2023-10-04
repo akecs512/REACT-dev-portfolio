7b950546b4ad88e508cf5d93146c188d3ad0cdec
Adrienne Kecskes <akecs619@yahoo.com>
2023-10-04 12:05:01 -0500

    error page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,7 +1,21 @@
+import myGif from "/worried.gif";
+
 export const ErrorPage = () => {
   return (
     <>
       <h1>Error</h1>
+
+      <div className="border">
+        <div>
+          <h2 className="error bg-accent">Awww... Don't Cry.</h2>
+          <div className="flex flex-col justify-items-center items-center">
+            <p>It's just a 404 error!</p>
+            <div className="worried flex justify-center">
+              <img src={myGif} alt="my-gif" />
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
