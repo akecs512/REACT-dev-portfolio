a7310dc6b5db19654c85578140d854f4f3fab9a9
Adrienne Kecskes <akecs619@yahoo.com>
2023-10-04 09:55:04 -0500

    resume page

--- src/pages/ResumePage.jsx
+++ src/pages/ResumePage.jsx
@@ -1,35 +1,18 @@
-import { pdfjs, Document, Page } from "react-pdf";
 
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  "pdfjs-dist/build/pdf.worker.min.js",
-  import.meta.url
-).toString();
 
 export const ResumePage = () => {
   return (
     <>
       <h1>Resume</h1>
-      <div className="container">
+      <div className="">
         <div className="flex flex-col items-start">
           <button className="btn btn-accent my-4 ">
-            <a href="/Adrienne Kecskes Resume 2023.pdf">
-              Download My Resume
-            </a>
+            <a href="/Adrienne Kecskes Resume 2023.pdf">Download My Resume</a>
           </button>
-          <Document file="/Adrienne Kecskes Resume 2023.pdf">
-            <div className="flex flex-row">
-              <Page
-                renderAnnotationLayer={false}
-                renderTextLayer={false}
-                pageNumber={1}
-              />
-              <Page
-                renderAnnotationLayer={false}
-                renderTextLayer={false}
-                pageNumber={2}
-              />
-            </div>
-          </Document>
+        </div>
+        <div className="px-20 mx-20">
+        < img src="/ResumePage1.png" alt="resume page 1" />
+        <img src="/ResumePage2.png" alt="resume page 2" />
         </div>
       </div>
     </>
